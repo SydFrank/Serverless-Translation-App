1e6148810aff5af3c63610a34f38c18a61f62e1b
SydFrank <frankzhsy@gmail.com>
2026-02-15 00:48:53 +1100

    translate api created and AccessDeniedException solved

--- infrastucture/lib/infrastucture-stack.ts
+++ infrastucture/lib/infrastucture-stack.ts
@@ -3,16 +3,24 @@ import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"; // Node.js Lambda function construct
 import * as apigateway from "aws-cdk-lib/aws-apigateway"; // API Gateway construct
+import * as iam from "aws-cdk-lib/aws-iam"; // IAM construct for permissions
 
 export class InfrastuctureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Define an IAM policy statement that allows the Lambda function to call the TranslateText API
+    const translateAccessPolicy = new iam.PolicyStatement({
+      actions: ["translate:TranslateText"],
+      resources: ["*"],
+    });
 
     // Create a Node.js Lambda function using the NodejsFunction construct
     const lambdaFunction = new lambdaNodeJS.NodejsFunction(this, "timeOfDay", {
       entry: "./lambda/timeOfDay.js",
       handler: "index",
       runtime: lambda.Runtime.NODEJS_22_X,
+      initialPolicy: [translateAccessPolicy], // Attach the IAM policy to the Lambda function
     });
 
     // Create an API Gateway REST API and integrate it with the Lambda function
